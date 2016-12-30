4867dff5535de9fdf230191047c7e60d54356e9f
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2016-12-29 23:50:22 -0500

    no message

---
+++ RealEstate/RealEstate/App_Start/RealEstateContext.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+using RealEstate.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RealEstate.App_Start
+{
+    public class RealEstateContext
+    {
+        public MongoDatabase Database { get; set; }
+
+        public RealEstateContext()
+        {
+            var client = new MongoClient(Settings.Default.ReadEstateConnectionString);
+            var server = client.GetServer();
+            Database = server.GetDatabase(Settings.Default.RealEstateDatabaseName);
+        }
+    }
+}
---
+++ RealEstate/RealEstate/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Driver;
+using RealEstate.App_Start;
+using RealEstate.Properties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RealEstate.Controllers
+{
+    public class HomeController : Controller
+    {
+        public RealEstateContext Context = new RealEstateContext();
+
+        public ActionResult Index()
+        {
+            Context.Database.GetStats();
+            return Json(Context.Database.Server.BuildInfo, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
