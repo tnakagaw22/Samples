8a2f042524a3bbf30161aedfa91495845df6ce88
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2017-02-01 00:12:19 -0500

    no message

---
+++ MyCodeCamp/src/MyCodeCamp/Controllers/CampsController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyCodeCamp.Data;
+using MyCodeCamp.Data.Entities;
+
+// For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyCodeCamp.Controllers
+{
+    [Route("api/[controller]")]
+    public class CampsController : Controller
+    {
+        private ICampRepository _repo;
+
+        public CampsController(ICampRepository repo)
+        {
+            _repo = repo;
+        }
+
+        // GET: api/{controller}
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            var camps = _repo.GetAllCamps();
+
+            return Ok(camps);
+        }
+
+        // GET api/{controller}/{id}
+        [HttpGet("{id}")]
+        public IActionResult Get(int id, bool includeSpeakers = false)
+        {
+            try
+            {
+                Camp camp = null;
+
+                if (includeSpeakers)
+                    camp = _repo.GetCampWithSpeakers(id);
+                else
+                    camp = _repo.GetCamp(id);
+
+                if (camp == null)
+                    return NotFound($"Camp {id} was not found");
+
+                return Ok(camp);
+            }
+            catch (Exception)
+            {
+
+            }
+
+            return BadRequest();
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
