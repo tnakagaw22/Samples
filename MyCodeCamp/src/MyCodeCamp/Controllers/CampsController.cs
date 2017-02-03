4f31d4d11e6e67a27be218d8b6190dab60684730
DESKTOP-TR8JSGT\tomohiro <tnakagaw22@hotmail.com>
2017-02-03 00:34:53 -0500

    no message

--- MyCodeCamp/src/MyCodeCamp/Controllers/CampsController.cs
+++ MyCodeCamp/src/MyCodeCamp/Controllers/CampsController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using MyCodeCamp.Data;
 using MyCodeCamp.Data.Entities;
+using Microsoft.Extensions.Logging;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -14,10 +15,12 @@ namespace MyCodeCamp.Controllers
     public class CampsController : Controller
     {
         private ICampRepository _repo;
+        private ILogger<CampsController> _logger;
 
-        public CampsController(ICampRepository repo)
+        public CampsController(ICampRepository repo, ILogger<CampsController> logger)
         {
             _repo = repo;
+            _logger = logger;
         }
 
         // GET: api/{controller}
@@ -30,7 +33,7 @@ namespace MyCodeCamp.Controllers
         }
 
         // GET api/{controller}/{id}
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "CampGet")]
         public IActionResult Get(int id, bool includeSpeakers = false)
         {
             try
@@ -57,20 +60,88 @@ namespace MyCodeCamp.Controllers
 
         // POST api/values
         [HttpPost]
-        public void Post([FromBody]string value)
+        public async Task<IActionResult> Post([FromBody]Camp model)
         {
+            try
+            {
+                _logger.LogInformation("Creating a new Code Camp");
+
+                _repo.Add(model);
+                if (await _repo.SaveAllAsync())
+                {
+                    var newUri = Url.Link("CampGet", new { id = model.Id });
+                    return Created(newUri, model);
+                }
+                else
+                {
+                    _logger.LogWarning("Could not save Camp to the database");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Threw exception while saving Camp : {ex}");
+            }
+
+            return BadRequest();
         }
 
         // PUT api/values/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
+        public async Task<IActionResult> Put(int id, [FromBody]Camp model)
         {
+            try
+            {
+                var oldCamp = _repo.GetCamp(id);
+                if (oldCamp == null)
+                    return NotFound($"Could not find a camp with an ID of {id}");
+
+                // Map model to the oldCamp
+                oldCamp.Name = model.Name ?? oldCamp.Name;
+                oldCamp.Description = model.Description ?? oldCamp.Description;
+                oldCamp.Location = model.Location ?? oldCamp.Location;
+                oldCamp.Length = model.Length > 0 ? model.Length : oldCamp.Length;
+                oldCamp.EventDate = model.EventDate != DateTime.MinValue ? model.EventDate : oldCamp.EventDate;
+
+                if (await _repo.SaveAllAsync())
+                {
+                    return Ok(oldCamp);
+                }
+                else
+                {
+                    _logger.LogWarning("Could not save Camp to the database");
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Threw exception while saving Camp : {ex}");
+            }
+
+            return BadRequest("Couldn't update Camp");
         }
 
         // DELETE api/values/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
+            try
+            {
+                var oldCamp = _repo.GetCamp(id);
+                if (oldCamp == null)
+                    return NotFound($"Could not find a camp with an ID of {id}");
+
+                _repo.Delete(oldCamp);
+
+                if (await _repo.SaveAllAsync())
+                {
+                    return Ok();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Threw exception while deleting Camp : {ex}");
+            }
+
+            return BadRequest("Couldn't delete Camp");
         }
     }
 }
